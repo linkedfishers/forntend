25588fd1de85c54b73e575ea4f1e6f8626709619
hamzanasri123 <hamzanasri16@gmail.com>
2021-07-05 16:24:26 +0100

    fix-admin-reb

--- src/app/components/common/navigation-widget/navigation-widget.component.ts
+++ src/app/components/common/navigation-widget/navigation-widget.component.ts
@@ -9,16 +9,17 @@ declare var initHexagons;
 @Component({
   selector: 'app-navigation-widget',
   templateUrl: './navigation-widget.component.html',
-  styleUrls: ['./navigation-widget.component.scss']
+  styleUrls: ['./navigation-widget.component.scss'],
 })
 export class NavigationWidgetComponent implements OnInit {
   readonly API: string = environment.apiUrl;
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private authService: AuthService,
-    private el: ElementRef,
-  ) { }
-  currentUser: User
+    private el: ElementRef
+  ) {}
+  currentUser: User;
   isAdmin = false;
   isProvider = false;
   fullName: string = '';
@@ -33,20 +34,21 @@ export class NavigationWidgetComponent implements OnInit {
       } else {
         this.fullName = this.currentUser.fullName;
       }
-      let profilePicture1, profilePicture2;
-      profilePicture1 = this.el.nativeElement.querySelector(
-        '#profilePicture1'
+      /*      let profilePicture1, profilePicture2;
+      profilePicture1 = this.el.nativeElement.querySelector('#profilePicture1');
+      profilePicture2 = this.el.nativeElement.querySelector('#profilePicture2');
+      profilePicture1.setAttribute(
+        'data-src',
+        picturePipe.transform(this.currentUser.profilePicture)
       );
-      profilePicture2 = this.el.nativeElement.querySelector(
-        '#profilePicture2'
-      );
-      profilePicture1.setAttribute('data-src', picturePipe.transform(this.currentUser.profilePicture));
-      profilePicture2.setAttribute('data-src', picturePipe.transform(this.currentUser.profilePicture));
+      profilePicture2.setAttribute(
+        'data-src',
+        picturePipe.transform(this.currentUser.profilePicture)
+      ); */
     } else {
       this.fullName = 'Guest';
     }
     initHexagons();
     this.isAdmin = this.authService.isAdmin();
   }
-
 }
