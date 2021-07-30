69a5271d1881e54f38cdaf2e7ea072681ce63d74
hamzanasri123 <hamzanasri16@gmail.com>
2021-07-30 16:26:20 +0100

    fixed error

--- src/app/components/pages/servicesbycat/servicesbycat.component.ts
+++ src/app/components/pages/servicesbycat/servicesbycat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { Service, ServiceType } from 'src/app/interfaces/equipments.interface';
 import { EquipmentService } from 'src/app/services/equipment.service';
 declare var initForm, $: any;
@@ -13,20 +14,26 @@ export class ServicesbycatComponent implements OnInit {
 
   constructor(
     private route : ActivatedRoute,
-    private equipmentService :EquipmentService
+    private equipmentService :EquipmentService,
+    private translate : TranslateService
 
   ) { }
       services : Service[]
       type:ServiceType
+      language:string
   ngOnInit(): void {
      initSidebar();
     initPopups();
     initForm();
     loadSvg();
+        this.language = this.translate.currentLang;
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.language = event.lang;
+    });
     this.route.params.subscribe(params=>{
       let id = params.id
       this.equipmentService.getServicessByType(id).subscribe(res=>{
-        this.services = res.data
+        this.services= res.data;
         console.log(this.services)
       })
     })
