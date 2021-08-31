4e06ebc6031dfb7313135f1f41e04a4391041209
hamzanasri123 <hamzanasri16@gmail.com>
2021-08-31 01:36:51 +0100

    home page

--- src/app/components/pages/details-equipment/details-equipment.component.ts
+++ src/app/components/pages/details-equipment/details-equipment.component.ts
@@ -1,48 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { Equipment, EquipmentType } from 'src/app/interfaces/equipments.interface';
+import {
+  Equipment,
+  EquipmentType,
+} from 'src/app/interfaces/equipments.interface';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/interfaces/users.interface';
 import { AuthService } from 'src/app/services/auth.service';
 import { EquipmentService } from 'src/app/services/equipment.service';
 import { environment } from 'src/environments/environment';
 
-declare var app, loadSvg, initTooltips,
+declare var app,
+  loadSvg,
+  initTooltips,
   initCharts,
   initHexagons,
   initPopups,
   initHeader,
   initContent,
-  initLoader, loadSvg: any;
+  initLoader,
+  loadSvg: any;
 
 @Component({
   selector: 'app-details-equipment',
   templateUrl: './details-equipment.component.html',
-  styleUrls: ['./details-equipment.component.scss']
+  styleUrls: ['./details-equipment.component.scss'],
 })
 export class DetailsEquipmentComponent implements OnInit {
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService,
-    private equipmentService: EquipmentService,
-  ) { }
+    private equipmentService: EquipmentService
+  ) {}
   readonly API: string = environment.apiUrl + '/';
 
   equipment: Equipment;
-  currentUser: User
+  currentUser: User;
   ngOnInit(): void {
-
-
     this.currentUser = this.authService.getCurrentUser();
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       let id = params.id;
       this.equipmentService.getEquipment(id).subscribe((response) => {
         this.equipment = response.data;
-      })
+        console.log(this.equipment);
+      });
     });
     initContent();
-
   }
-
 }
