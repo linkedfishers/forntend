10d5048dfb8cb1909afa4d10ae254fdb53c813dd
hamzanasri123 <hamzanasri16@gmail.com>
2021-10-09 19:12:30 +0100

    test

--- src/app/components/pages/mainpage/mainpage.component.ts
+++ src/app/components/pages/mainpage/mainpage.component.ts
@@ -1,8 +1,15 @@
 import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import {
+  Boat,
+  Equipment,
+  Hebergement,
+  Service,
+} from 'src/app/interfaces/equipments.interface';
 import { Categorie, Product } from 'src/app/interfaces/product.interface';
 import { AdminService } from 'src/app/services/admin.service';
+import { EquipmentService } from 'src/app/services/equipment.service';
 import ProductService from 'src/app/services/product.service';
 import { environment } from 'src/environments/environment';
 
@@ -15,10 +22,14 @@ export class MainpageComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private adminservice: AdminService,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private equipmentService: EquipmentService
   ) {}
   readonly API: string = environment.apiUrl + '/';
-
+  boats: Boat[];
+  equipments: Equipment[];
+  hebergements: Hebergement[];
+  services: Service[];
   categories: Categorie[];
   contenttext: Content;
   products: Product[];
@@ -35,6 +46,19 @@ export class MainpageComponent implements OnInit {
     this.productService.getProducts().subscribe((response) => {
       this.products = response.data;
       this.visibleProducts = this.products;
+    });
+    this.equipmentService.getEquipments().subscribe((response) => {
+      this.equipments = response.data;
+      console.log(this.equipments);
+    });
+    this.equipmentService.getBoats().subscribe((reponse) => {
+      this.boats = reponse.data;
+    });
+    this.equipmentService.getHebergements().subscribe((reponse) => {
+      this.hebergements = reponse.data;
+    });
+    this.equipmentService.getServices().subscribe((reponse) => {
+      this.services = reponse.data;
     });
   }
 }
