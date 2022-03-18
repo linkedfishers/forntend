82664557f3b4feac90714b1bdf7c8574af7f02d3
hamzanasri123 <hamzanasri16@gmail.com>
2022-03-18 16:43:29 +0100

    homePageDes

--- src/app/components/pages/visiteguider/visiteguider.component.ts
+++ src/app/components/pages/visiteguider/visiteguider.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { IpServiceService } from 'src/app/services/ip-service.service';
 import { ReservationService } from 'src/app/services/reservation.service';
 import * as AOS from 'aos';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+
 
 @Component({
   selector: 'app-visiteguider',
@@ -9,9 +12,17 @@ import * as AOS from 'aos';
   styleUrls: ['./visiteguider.component.scss'],
 })
 export class VisiteguiderComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router, viewportScroller: ViewportScroller) {}
+
+  
 
   ngOnInit(): void {
     AOS.init();
   }
+
+
+
+
+
+  navigate = () => this.router.navigate(['/acceuil'], { fragment: 'team' });
 }
